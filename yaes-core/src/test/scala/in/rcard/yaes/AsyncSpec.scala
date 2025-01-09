f543d714814dcf1d820f7be4384c7478ba6150be
rcardin <riccardo.cardin@gmail.com>
2025-01-09 14:31:24 +0100

    refactor: Rename 'job' to 'fiber' in AsyncSpec for consistency

--- yaes-core/src/test/scala/in/rcard/yaes/AsyncSpec.scala
+++ yaes-core/src/test/scala/in/rcard/yaes/AsyncSpec.scala
@@ -10,95 +10,95 @@ import org.scalatest.TryValues.*
 import scala.util.Try
 
 class AsyncSpec extends AnyFlatSpec with Matchers {
-  "The Async effect" should "wait the completion of all the forked jobs" in {
+  "The Async effect" should "wait the completion of all the forked fibers" in {
     val results = Async.run {
       val queue = new ConcurrentLinkedQueue[String]()
-      val job1 = Async.fork {
+      val fb1 = Async.fork {
         Async.delay(1.second)
-        queue.add("job1")
+        queue.add("fb1")
       }
-      val job2 = Async.fork {
+      val fb2 = Async.fork {
         Async.delay(500.millis)
-        queue.add("job2")
+        queue.add("fb2")
       }
-      val job3 = Async.fork {
+      val fb3 = Async.fork {
         Async.delay(100.millis)
-        queue.add("job3")
+        queue.add("fb3")
       }
       queue
     }
 
-    results.toArray should contain theSameElementsInOrderAs List("job3", "job2", "job1")
+    results.toArray should contain theSameElementsInOrderAs List("fb3", "fb2", "fb1")
   }
 
-  it should "stop the execution if one the job throws an exception" in {
+  it should "stop the execution if one the fiber throws an exception" in {
     val results = new ConcurrentLinkedQueue[String]()
     val tryResult = Try {
       Async.run {
-        val job1 = Async.fork {
+        val fb1 = Async.fork {
           Async.delay(1.second)
-          results.add("job1")
+          results.add("fb1")
         }
-        val job2 = Async.fork {
+        val fb2 = Async.fork {
           Async.delay(500.millis)
-          results.add("job2")
+          results.add("fb2")
           throw new RuntimeException("Error")
         }
-        val job3 = Async.fork {
+        val fb3 = Async.fork {
           Async.delay(100.millis)
-          results.add("job3")
+          results.add("fb3")
         }
       }
     }
 
     tryResult.failure.exception shouldBe a[RuntimeException]
     tryResult.failure.exception.getMessage shouldBe "Error"
-    results.toArray should contain theSameElementsInOrderAs List("job3", "job2")
+    results.toArray should contain theSameElementsInOrderAs List("fb3", "fb2")
   }
 
-  it should "stop the execution if a child job throws an exception" in {
+  it should "stop the execution if a child fiber throws an exception" in {
     val results = new ConcurrentLinkedQueue[String]()
     val tryResult = Try {
       Async.run {
-        val job1 = Async.fork {
+        val fb1 = Async.fork {
           Async.delay(1.second)
-          results.add("job1")
+          results.add("fb1")
         }
-        val job2 = Async.fork {
+        val fb2 = Async.fork {
           Async.delay(500.millis)
-          results.add("job2")
+          results.add("fb2")
           Async.fork {
             Async.delay(100.millis)
             throw new RuntimeException("Error")
           }
         }
-        val job3 = Async.fork {
+        val fb3 = Async.fork {
           Async.delay(100.millis)
-          results.add("job3")
+          results.add("fb3")
         }
       }
     }
 
     tryResult.failure.exception shouldBe a[RuntimeException]
     tryResult.failure.exception.getMessage shouldBe "Error"
-    results.toArray should contain theSameElementsInOrderAs List("job3", "job2")
+    results.toArray should contain theSameElementsInOrderAs List("fb3", "fb2")
   }
 
   it should "stop the execution if the block throws an exception" in {
     val results = new ConcurrentLinkedQueue[String]()
     val tryResult = Try {
       Async.run {
-        val job1 = Async.fork {
+        val fb1 = Async.fork {
           Async.delay(1.second)
-          results.add("job1")
+          results.add("fb1")
         }
-        val job2 = Async.fork {
+        val fb2 = Async.fork {
           Async.delay(500.millis)
-          results.add("job2")
+          results.add("fb2")
         }
-        val job3 = Async.fork {
+        val fb3 = Async.fork {
           Async.delay(100.millis)
-          results.add("job3")
+          results.add("fb3")
         }
         throw new RuntimeException("Error")
       }
@@ -109,30 +109,30 @@ class AsyncSpec extends AnyFlatSpec with Matchers {
     results.toArray shouldBe empty
   }
 
-  it should "join the values of different jobs" in {
+  it should "join the values of different fibers" in {
     val queue = new ConcurrentLinkedQueue[String]()
     val result = Async.run {
-      val job1 = Async.fork {
+      val fb1 = Async.fork {
         Async.delay(1.second)
-        queue.add("job1")
+        queue.add("fb1")
         42
       }
-      val job2 = Async.fork {
+      val fb2 = Async.fork {
         Async.delay(500.millis)
-        queue.add("job2")
+        queue.add("fb2")
         43
       }
-      job1.value + job2.value
+      fb1.value + fb2.value
     }
 
-    queue.toArray should contain theSameElementsInOrderAs List("job2", "job1")
+    queue.toArray should contain theSameElementsInOrderAs List("fb2", "fb1")
     result shouldBe 85
   }
 
-  it should "wait for children jobs to finish" in {
+  it should "wait for children fibers to finish" in {
     val results = Async.run {
       val queue = new ConcurrentLinkedQueue[String]()
-      val job1 = Async.fork {
+      val fb1 = Async.fork {
         Async.fork {
           Async.delay(1.second)
           queue.add("1")
