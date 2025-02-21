b360b1a920070c50d304c4b4d13a840f868f7f51
rcardin <riccardo.cardin@gmail.com>
2025-02-21 17:20:43 +0100

    Added the handle..with syntax

--- yaes-core/src/main/scala/in/rcard/yaes/Effect.scala
+++ yaes-core/src/main/scala/in/rcard/yaes/Effect.scala
@@ -1,13 +1,30 @@
 package in.rcard.yaes
 
-/**
- * Represents an effect that handles the execution of a side-effecting operation.
- * @param sf The side-effecting operation to be executed.
- * @tparam F The type of the side-effecting operation.
- */
+/** Represents an effect that handles the execution of a side-effecting operation.
+  * @param sf
+  *   The side-effecting operation to be executed.
+  * @tparam F
+  *   The type of the side-effecting operation.
+  */
 class Effect[F](val sf: F)
 
 extension [F, A](inline eff: Effect[F] ?=> A) {
-  inline def map[B](inline f: A => B): Effect[F] ?=> B = eff.flatMap(a => f(a))
+  inline def map[B](inline f: A => B): Effect[F] ?=> B                   = eff.flatMap(a => f(a))
   inline def flatMap[B](inline f: A => Effect[F] ?=> B): Effect[F] ?=> B = f(eff)
+}
+
+object Effect {
+  def handle[F, A](program: Effect[F] ?=> A) = new WithHandler[F, A] {
+    override def `with`(handler: Handler[F]): A = {
+      program(using new Effect(handler.unsafe))
+    }
+  }
+
+  trait WithHandler[F, A] {
+    def `with`(handler: Handler[F]): A
+  }
+
+  trait Handler[F] {
+    val unsafe: F
+  }
 }
