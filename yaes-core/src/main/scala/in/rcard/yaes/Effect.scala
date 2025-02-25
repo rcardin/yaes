a11fa76ba9a69bbe57a8a433723778df9c0bdad6
rcardin <riccardo.cardin@gmail.com>
2025-02-25 09:07:07 +0100

    refactor: Removed the handle..with DSL (for now)

--- yaes-core/src/main/scala/in/rcard/yaes/Effect.scala
+++ yaes-core/src/main/scala/in/rcard/yaes/Effect.scala
@@ -12,19 +12,3 @@ extension [F, A](inline eff: Effect[F] ?=> A) {
   inline def map[B](inline f: A => B): Effect[F] ?=> B                   = eff.flatMap(a => f(a))
   inline def flatMap[B](inline f: A => Effect[F] ?=> B): Effect[F] ?=> B = f(eff)
 }
-
-object Effect {
-  def handle[F, A](program: Effect[F] ?=> A) = new WithHandler[F, A] {
-    override def `with`(handler: Handler[F]): A = {
-      program(using new Effect(handler.unsafe))
-    }
-  }
-
-  trait WithHandler[F, A] {
-    def `with`(handler: Handler[F]): A
-  }
-
-  trait Handler[F] {
-    val unsafe: F
-  }
-}
