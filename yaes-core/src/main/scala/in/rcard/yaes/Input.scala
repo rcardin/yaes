992710a608fa9d421d9c30fd81e23d4f7037b8c3
rcardin <riccardo.cardin@gmail.com>
2025-03-06 16:51:23 +0100

    refactor: Update Input trait to use Yaes type

--- yaes-core/src/main/scala/in/rcard/yaes/Input.scala
+++ yaes-core/src/main/scala/in/rcard/yaes/Input.scala
@@ -4,23 +4,6 @@ import in.rcard.yaes.Raise.Raise
 
 import java.io.IOException
 
-/** A capability trait representing console input operations in a capability-passing style.
-  *
-  * The `Input` trait provides a safe abstraction for reading input from the console while handling
-  * potential IOExceptions through the effect system.
-  *
-  * Example:
-  * {{{
-  * def readName()(using input: Input, output: Output, raise: Raise[IOException]): String =
-  *   output.println("What's your name?")
-  *   val name = input.readLn()
-  *   output.println(s"Hello, $name!")
-  * }}}
-  */
-trait Input extends Effect {
-  def readLn()(using t: Raise[IOException]): String
-}
-
 /** Companion object for the Input capability, providing utility methods and handlers.
   *
   * This object contains methods to run Input operations and a default unsafe implementation that
@@ -38,6 +21,8 @@ trait Input extends Effect {
   * }}}
   */
 object Input {
+
+  type Input = Yaes[Input.Unsafe]
 
   /** Lifts a block of code into the Input effect.
     *
@@ -57,7 +42,7 @@ object Input {
     * @return
     *   The line of input read from the console
     */
-  def readLn()(using input: Input)(using t: Raise[IOException]): String = input.readLn()
+  def readLn()(using input: Input)(using t: Raise[IOException]): String = input.unsafe.readLn()
 
   /** Runs a program that requires Input capability.
     *
@@ -77,13 +62,13 @@ object Input {
     *   The result of the program
     */
   def run[A](block: Input ?=> A): A = {
-    val handler = new Effect.Handler[Input, A, A] {
-      override def handle(program: Input ?=> A): A = program(using Input.unsafe)
+    val handler = new Yaes.Handler[Input.Unsafe, A, A] {
+      override def handle(program: Input ?=> A): A = program(using Yaes(Input.unsafe))
     }
-    Effect.handle(block)(using handler)
+    Yaes.handle(block)(using handler)
   }
 
-  val unsafe = new Input {
+  val unsafe = new Input.Unsafe {
     override def readLn()(using t: Raise[IOException]): String = Raise {
       try {
         scala.io.StdIn.readLine()
@@ -92,5 +77,22 @@ object Input {
           Raise.raise(e)
       }
     }
+  }
+
+  /** A capability trait representing console input operations in a capability-passing style.
+    *
+    * The `Input` trait provides a safe abstraction for reading input from the console while
+    * handling potential IOExceptions through the effect system.
+    *
+    * Example:
+    * {{{
+    * def readName()(using input: Input, output: Output, raise: Raise[IOException]): String =
+    *   output.println("What's your name?")
+    *   val name = input.readLn()
+    *   output.println(s"Hello, $name!")
+    * }}}
+    */
+  trait Unsafe extends Eff {
+    def readLn()(using t: Raise[IOException]): String
   }
 }
