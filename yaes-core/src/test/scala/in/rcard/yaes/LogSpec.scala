04e2c5c27682f9444b15e7c35143d8c959292905
rcardin <riccardo.cardin@gmail.com>
2025-04-23 12:54:19 +0200

    feat(25): Added unit tests

--- yaes-core/src/test/scala/in/rcard/yaes/LogSpec.scala
+++ yaes-core/src/test/scala/in/rcard/yaes/LogSpec.scala
@@ -18,7 +18,7 @@ class LogSpec extends AnyFlatSpec with Matchers {
   given fixedClock: JClock =
     JClock.fixed(FIXED_TIME, ZoneId.of("UTC"))
 
-  "The Log effect" should "be able to log at trace level" in {
+  "The Log effect" should "log at TRACE level" in {
     val actualResult = new ByteArrayOutputStream()
     Console.withOut(actualResult) {
       Log.run {
@@ -33,9 +33,135 @@ class LogSpec extends AnyFlatSpec with Matchers {
       }
     }
 
-    val output = actualResult.toString
-    output should include("TestLogger")
-    output should include("TRACE")
-    output should include("Trace message")
+    val actualLoggedLines = actualResult.toString.split('\n')
+
+    actualLoggedLines should contain allOf (
+      "2025-04-22T19:55:59 - TRACE - TestLogger - Trace message",
+      "2025-04-22T19:55:59 - DEBUG - TestLogger - Debug message",
+      "2025-04-22T19:55:59 - INFO - TestLogger - Info message",
+      "2025-04-22T19:55:59 - WARN - TestLogger - Warn message",
+      "2025-04-22T19:55:59 - ERROR - TestLogger - Error message",
+      "2025-04-22T19:55:59 - FATAL - TestLogger - Fatal message"
+    )
+  }
+
+  it should "log at DEBUG level" in {
+    val actualResult = new ByteArrayOutputStream()
+    Console.withOut(actualResult) {
+      Log.run {
+        val logger = Log.getLogger("TestLogger", Log.Level.Debug)
+
+        logger.trace("Trace message")
+        logger.debug("Debug message")
+        logger.info("Info message")
+        logger.warn("Warn message")
+        logger.error("Error message")
+        logger.fatal("Fatal message")
+      }
+    }
+
+    val actualLoggedLines = actualResult.toString.split('\n')
+
+    actualLoggedLines should contain allOf (
+      "2025-04-22T19:55:59 - DEBUG - TestLogger - Debug message",
+      "2025-04-22T19:55:59 - INFO - TestLogger - Info message",
+      "2025-04-22T19:55:59 - WARN - TestLogger - Warn message",
+      "2025-04-22T19:55:59 - ERROR - TestLogger - Error message",
+      "2025-04-22T19:55:59 - FATAL - TestLogger - Fatal message"
+    )
+  }
+
+  it should "log at INFO level" in {
+    val actualResult = new ByteArrayOutputStream()
+    Console.withOut(actualResult) {
+      Log.run {
+        val logger = Log.getLogger("TestLogger", Log.Level.Info)
+
+        logger.trace("Trace message")
+        logger.debug("Debug message")
+        logger.info("Info message")
+        logger.warn("Warn message")
+        logger.error("Error message")
+        logger.fatal("Fatal message")
+      }
+    }
+
+    val actualLoggedLines = actualResult.toString.split('\n')
+
+    actualLoggedLines should contain allOf (
+      "2025-04-22T19:55:59 - INFO - TestLogger - Info message",
+      "2025-04-22T19:55:59 - WARN - TestLogger - Warn message",
+      "2025-04-22T19:55:59 - ERROR - TestLogger - Error message",
+      "2025-04-22T19:55:59 - FATAL - TestLogger - Fatal message"
+    )
+  }
+
+  it should "log at WARN level" in {
+    val actualResult = new ByteArrayOutputStream()
+    Console.withOut(actualResult) {
+      Log.run {
+        val logger = Log.getLogger("TestLogger", Log.Level.Warn)
+
+        logger.trace("Trace message")
+        logger.debug("Debug message")
+        logger.info("Info message")
+        logger.warn("Warn message")
+        logger.error("Error message")
+        logger.fatal("Fatal message")
+      }
+    }
+
+    val actualLoggedLines = actualResult.toString.split('\n')
+
+    actualLoggedLines should contain allOf (
+      "2025-04-22T19:55:59 - WARN - TestLogger - Warn message",
+      "2025-04-22T19:55:59 - ERROR - TestLogger - Error message",
+      "2025-04-22T19:55:59 - FATAL - TestLogger - Fatal message"
+    )
+  }
+
+  it should "log at ERROR level" in {
+    val actualResult = new ByteArrayOutputStream()
+    Console.withOut(actualResult) {
+      Log.run {
+        val logger = Log.getLogger("TestLogger", Log.Level.Error)
+
+        logger.trace("Trace message")
+        logger.debug("Debug message")
+        logger.info("Info message")
+        logger.warn("Warn message")
+        logger.error("Error message")
+        logger.fatal("Fatal message")
+      }
+    }
+
+    val actualLoggedLines = actualResult.toString.split('\n')
+
+    actualLoggedLines should contain allOf (
+      "2025-04-22T19:55:59 - ERROR - TestLogger - Error message",
+      "2025-04-22T19:55:59 - FATAL - TestLogger - Fatal message"
+    )
+  }
+
+  it should "log at FATAL level" in {
+    val actualResult = new ByteArrayOutputStream()
+    Console.withOut(actualResult) {
+      Log.run {
+        val logger = Log.getLogger("TestLogger", Log.Level.Fatal)
+
+        logger.trace("Trace message")
+        logger.debug("Debug message")
+        logger.info("Info message")
+        logger.warn("Warn message")
+        logger.error("Error message")
+        logger.fatal("Fatal message")
+      }
+    }
+
+    val actualLoggedLines = actualResult.toString.split('\n')
+
+    actualLoggedLines should contain(
+      "2025-04-22T19:55:59 - FATAL - TestLogger - Fatal message"
+    )
   }
 }
