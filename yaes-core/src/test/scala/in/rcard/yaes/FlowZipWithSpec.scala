a0e235233d1924024daa87f23290b41a08a1dff2
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2025-05-16 18:32:01 +0200

    Created an unified generator of flows and refactored tests
    Replaced granular flow generators with a unified `genFlow` method to simplify test logic and ensure consistency. Updated tests to cover multiple edge cases and enhance readability while maintaining comprehensive coverage of `zipWith` behavior.

--- yaes-core/src/test/scala/in/rcard/yaes/FlowZipWithSpec.scala
+++ yaes-core/src/test/scala/in/rcard/yaes/FlowZipWithSpec.scala
@@ -18,135 +18,104 @@ class FlowZipWithSpec extends AnyFlatSpec with Matchers with ScalaCheckPropertyC
       .map(_ * 1000 * 1000) // to millis
       .map(Duration.fromNanos)
 
-  // I disable the shrinking for the generator of Durations
-  implicit val noShrink: Shrink[Duration] = Shrink.shrinkAny[Duration]
-
-  private def delayedEmpty(t1: Duration)(using async: Async): Flow[Int] = Flow.flow {
-    Async.delay(t1)
-  }
-
-  private def delayedOne[A](t1: Duration, t2: Duration)(a: A)(using async: Async): Flow[A] =
-    Flow.flow {
-      Async.delay(t1)
-      Flow.emit(a)
-      Async.delay(t2)
+  private def genFlow[A](as: A*)(using async: Async): Gen[Flow[A]] =
+    for {
+      delays <- Gen.listOfN(as.length + 1, genMillis)
+    } yield Flow.flow[A] {
+      delays.zip(as :+ null).foreach { case (delay, element) =>
+        Async.delay(delay)
+        if (element != null) Flow.emit(element.nn)
+      }
     }
 
-  private def delayedTwo[A](t1: Duration, t2: Duration, t3: Duration)(a1: A, a2: A)(using
-      async: Async
-  ): Flow[A] = Flow.flow {
-    Async.delay(t1)
-    Flow.emit(a1)
-    Async.delay(t2)
-    Flow.emit(a2)
-    Async.delay(t3)
-  }
-
   "zipWith" should "zip two empty flows" in {
-    forAll(genMillis, genMillis) { (delay1, delay2) =>
-      val actualQueue = new ConcurrentLinkedQueue[(Int, Int)]()
-      Async.run {
-        val flow1  = delayedEmpty(delay1)
-        val flow2  = delayedEmpty(delay2)
-        val zipped = flow1.zipWith(flow2)((_, _))
+    Async.run {
+      forAll(genFlow(), genFlow()) { (flow1, flow2) =>
+        val actualQueue = new ConcurrentLinkedQueue[(Any, Any)]()
+        val zipped      = flow1.zipWith(flow2)((_, _))
         zipped.collect {
           actualQueue.add(_)
         }
+        actualQueue shouldBe empty
       }
-      actualQueue shouldBe empty
     }
   }
 
   it should "zip one empty flow with one non empty" in {
-    forAll(genMillis, genMillis, genMillis) { (delay1, delay2, delay3) =>
-      val actualQueue = new ConcurrentLinkedQueue[(Int, Int)]()
-      Async.run {
-        val flow1  = delayedEmpty(delay1)
-        val flow2  = delayedOne(delay2, delay3)(1)
-        val zipped = flow1.zipWith(flow2)((_, _))
+    Async.run {
+      forAll(genFlow(), genFlow('A')) { (flow1, flow2) =>
+        val actualQueue = new ConcurrentLinkedQueue[(Any, Char)]()
+        val zipped      = flow1.zipWith(flow2)((_, _))
         zipped.collect {
           actualQueue.add(_)
         }
+        actualQueue shouldBe empty
       }
-      actualQueue shouldBe empty
     }
   }
 
   it should "zip one non empty with one empty" in {
-    forAll(genMillis, genMillis, genMillis) { (delay1, delay2, delay3) =>
-      val actualQueue = new ConcurrentLinkedQueue[(Int, Int)]()
-      Async.run {
-        val flow1  = delayedOne(delay1, delay2)(1)
-        val flow2  = delayedEmpty(delay3)
-        val zipped = flow1.zipWith(flow2)((_, _))
+    Async.run {
+      forAll(genFlow('A'), genFlow()) { (flow1, flow2) =>
+        val actualQueue = new ConcurrentLinkedQueue[(Char, Any)]()
+        val zipped      = flow1.zipWith(flow2)((_, _))
         zipped.collect {
           actualQueue.add(_)
         }
+        actualQueue shouldBe empty
       }
-      actualQueue shouldBe empty
     }
   }
 
   it should "zip to flows of one element" in {
-    forAll(genMillis, genMillis, genMillis, genMillis) { (delay1, delay2, delay3, delay4) =>
-      val actualQueue = new ConcurrentLinkedQueue[(Char, Int)]()
-      Async.run {
-        val flow1  = delayedOne(delay1, delay2)('A')
-        val flow2  = delayedOne(delay3, delay4)(1)
-        val zipped = flow1.zipWith(flow2)((_, _))
+    Async.run {
+      forAll(genFlow('A'), genFlow(1)) { (flow1, flow2) =>
+        val actualQueue = new ConcurrentLinkedQueue[(Char, Int)]()
+        val zipped      = flow1.zipWith(flow2)((_, _))
         zipped.collect {
           actualQueue.add(_)
         }
+        actualQueue.toArray should contain theSameElementsInOrderAs Seq('A' -> 1)
       }
-      actualQueue.toArray should contain theSameElementsInOrderAs Seq('A' -> 1)
     }
   }
 
   it should "zip a flow with one element with another with two" in {
-    forAll(genMillis, genMillis, genMillis, genMillis, genMillis) {
-      (delay1, delay2, delay3, delay4, delay5) =>
+    Async.run {
+      forAll(genFlow('A'), genFlow(1, 2)) { (flow1, flow2) =>
         val actualQueue = new ConcurrentLinkedQueue[(Char, Int)]()
-        Async.run {
-          lazy val flow1 = delayedOne(delay1, delay2)('A')
-          lazy val flow2 = delayedTwo(delay3, delay4, delay5)(1, 2)
-          val zipped     = flow1.zipWith(flow2)((_, _))
-          zipped.collect {
-            actualQueue.add(_)
-          }
+        val zipped      = flow1.zipWith(flow2)((_, _))
+        zipped.collect {
+          actualQueue.add(_)
         }
         actualQueue.toArray should contain theSameElementsInOrderAs Seq('A' -> 1)
+      }
     }
   }
 
   it should "zip a flow with two elements with another with one" in {
-    forAll(genMillis, genMillis, genMillis, genMillis, genMillis) {
-      (delay1, delay2, delay3, delay4, delay5) =>
+    Async.run {
+      forAll(genFlow('A', 'B'), genFlow(1)) { (flow1, flow2) =>
         val actualQueue = new ConcurrentLinkedQueue[(Char, Int)]()
-        Async.run {
-          lazy val flow1 = delayedTwo(delay1, delay2, delay3)('A', 'B')
-          lazy val flow2 = delayedOne(delay3, delay4)(1)
-          val zipped     = flow1.zipWith(flow2)((_, _))
-          zipped.collect {
-            actualQueue.add(_)
-          }
+        val zipped      = flow1.zipWith(flow2)((_, _))
+        zipped.collect {
+          actualQueue.add(_)
         }
         actualQueue.toArray should contain theSameElementsInOrderAs Seq('A' -> 1)
+      }
     }
   }
 
   it should "zip two flows with two elements each" in {
-    forAll(genMillis, genMillis, genMillis, genMillis, genMillis, genMillis) {
-      (delay1, delay2, delay3, delay4, delay5, delay6) =>
+    Async.run {
+      forAll(genFlow('A', 'B'), genFlow(1, 2)) { (flow1, flow2) =>
         val actualQueue = new ConcurrentLinkedQueue[(Char, Int)]()
-        Async.run {
-          lazy val flow1 = delayedTwo(delay1, delay2, delay3)('A', 'B')
-          lazy val flow2 = delayedTwo(delay3, delay4, delay5)(1, 2)
-          val zipped     = flow1.zipWith(flow2)((_, _))
-          zipped.collect {
-            actualQueue.add(_)
-          }
+        val zipped      = flow1.zipWith(flow2)((_, _))
+        zipped.collect {
+          actualQueue.add(_)
         }
         actualQueue.toArray should contain theSameElementsInOrderAs Seq('A' -> 1, 'B' -> 2)
+      }
     }
   }
 }
