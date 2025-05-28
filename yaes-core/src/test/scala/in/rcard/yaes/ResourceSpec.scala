7e07792873bfa305bf74091c506c758d58048b8e
rcardin <riccardo.cardin@gmail.com>
2025-05-28 21:55:08 +0200

    feat(47): Added more tests

--- yaes-core/src/test/scala/in/rcard/yaes/ResourceSpec.scala
+++ yaes-core/src/test/scala/in/rcard/yaes/ResourceSpec.scala
@@ -19,4 +19,43 @@ class ResourceSpec extends AnyFlatSpec with Matchers {
     actualResource shouldEqual "Acquired"
     results shouldEqual List("Acquired", "Released")
   }
+
+  it should "release resources correctly if an error happens after the acquiring process" in {
+
+    val results = scala.collection.mutable.ListBuffer[String]()
+    val actualException = intercept[RuntimeException] {
+      Resource.run {
+        val acquired = Resource.install({
+          "Acquired"
+        }) { res =>
+          results += res
+          results += "Released"
+        }
+        throw new RuntimeException("An error occurred after acquiring the resource")
+      }
+    }
+
+    actualException shouldBe a[RuntimeException]
+    actualException.getMessage shouldEqual "An error occurred after acquiring the resource"
+    results shouldEqual List("Acquired", "Released")
+  }
+
+  it should "release resources correctly if an error happens during the acquiring process" in {
+
+    val results = scala.collection.mutable.ListBuffer[String]()
+    val actualException = intercept[RuntimeException] {
+      Resource.run {
+        Resource.install[String]({
+          throw new RuntimeException("An error occurred during acquiring the resource")
+        }) { res =>
+          results += res
+          results += "Released"
+        }
+      }
+    }
+
+    actualException shouldBe a[RuntimeException]
+    actualException.getMessage shouldEqual "An error occurred during acquiring the resource"
+    results shouldEqual List()
+  }
 }
