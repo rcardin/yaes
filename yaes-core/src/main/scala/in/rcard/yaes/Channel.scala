7056f35730faa634a93cfdaacfef94bf0e53edfb
rcardin <riccardo.cardin@gmail.com>
2025-10-13 10:25:32 +0200

    feat(Channel): Implemented a raw version of the unbounded Channel (without close operation)

--- yaes-core/src/main/scala/in/rcard/yaes/Channel.scala
+++ yaes-core/src/main/scala/in/rcard/yaes/Channel.scala
@@ -1,15 +1,33 @@
 package in.rcard.yaes
 
 import in.rcard.yaes.Async.Async
+import in.rcard.yaes.Channel.SendChannel
+import java.util.concurrent.BlockingQueue
 
 object Channel {
 
   trait SendChannel[T] {
     def send(value: T)(using Async): Unit
-    def close(): Unit
+    def close(): Boolean
   }
 
   trait ReceiveChannel[T] {
     def receive()(using Async): T
   }
+
+  def unbounded[T](): Channel[T] =
+    new Channel(new java.util.concurrent.LinkedBlockingQueue[T]())
+}
+
+class Channel[T] private(queue: BlockingQueue[T]) extends Channel.ReceiveChannel[T], Channel.SendChannel[T] {
+
+  override def receive()(using Async): T = 
+    queue.take()
+
+  override def send(value: T)(using Async): Unit =
+    queue.put(value)
+
+  override def close(): Boolean = ???
+
+
 }
---
+++ yaes-core/src/test/scala/in/rcard/yaes/ChannelSpec.scala
@@ -0,0 +1,60 @@
+package in.rcard.yaes
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import scala.concurrent.duration._
+
+class ChannelSpec extends AnyFlatSpec with Matchers {
+
+  "A Channel" should "send and receive values correctly" in {
+    val channel             = Channel.unbounded[Int]()
+    var actualReceivedValue = 0
+
+    Async.run {
+      Async.fork {
+        channel.send(42)
+      }
+
+      val fiber = Async.fork {
+        actualReceivedValue = channel.receive()
+      }
+    }
+
+    actualReceivedValue should be(42)
+  }
+
+  it should "block on receive when no value is available" in {
+    val channel             = Channel.unbounded[Int]()
+    var actualReceivedValue = 0
+
+    Async.run {
+      val fiber = Async.fork {
+        actualReceivedValue = channel.receive()
+      }
+
+      Async.delay(100.millis)
+      channel.send(42)
+    }
+
+    actualReceivedValue should be(42)
+  }
+
+//   it should "not discard messages sent before closing" in {
+//     val channel = Channel.unbounded[Int]()
+
+//     Async.run {
+//       Async.fork {
+//         channel.send(1)
+//         channel.send(2)
+//         channel.send(3)
+//         channel.close()
+//       }
+
+//       Async.delay(200.millis)
+
+//       val actualResult = channel.receive() + channel.receive() + channel.receive()
+
+//       actualResult should be(6)
+//     }
+//   }
+}
