fca912daa32275433130f7fd2c3d69b5758bab62
rcardin <riccardo.cardin@gmail.com>
2025-12-09 21:24:49 +0100

    feat: create syntax.all combining all syntax extensions
    Add unified syntax object following Cats pattern for importing all
    extensions at once.
    
    Part of yaes-cats reorganization (#126)

--- yaes-cats/src/main/scala/in/rcard/yaes/syntax/package.scala
+++ yaes-cats/src/main/scala/in/rcard/yaes/syntax/package.scala
@@ -8,45 +8,19 @@ package in.rcard.yaes
   * Available syntax:
   *  - `accumulate` - Error accumulation extensions (`combineErrors`, `combineErrorsS`)
   *  - `validated` - Cats Validated extensions (`value`)
-  *  - `catsIO` - Cats Effect IO extensions (`value`)
+  *  - `catseffect` - Cats Effect IO extensions (`value`)
   *  - `all` - All of the above
   *
   * Example:
   * {{{
   * // Import specific syntax
-  * import in.rcard.yaes.syntax.accumulate.*
-  * import in.rcard.yaes.syntax.validated.*
-  * import in.rcard.yaes.syntax.catsIO.*
+  * import in.rcard.yaes.syntax.accumulate.given
+  * import in.rcard.yaes.syntax.validated.given
+  * import in.rcard.yaes.syntax.catseffect.given
   *
   * // Or import all syntax
-  * import in.rcard.yaes.syntax.all.*
+  * import in.rcard.yaes.syntax.all.given
   * }}}
   */
-package object syntax {
+package object syntax
 
-  /** Object providing all syntax extensions.
-    *
-    * Import from this object to get all extension methods:
-    * - `combineErrors`, `combineErrorsS` for error accumulation
-    * - `value` for Cats Validated types
-    * - `value` for Cats Effect IO
-    *
-    * Example:
-    * {{{
-    * import in.rcard.yaes.syntax.all.*
-    * import cats.effect.{IO => CatsIO}
-    * import cats.data.Validated
-    *
-    * val catsIO: CatsIO[Int] = CatsIO.pure(42)
-    * val validated: Validated[String, Int] = Validated.valid(42)
-    *
-    * YaesIO.run {
-    *   Raise.either {
-    *     catsIO.value      // From catsIO syntax
-    *     validated.value   // From validated syntax
-    *   }
-    * }
-    * }}}
-    */
-  object all extends AccumulateSyntax with ValidatedSyntax with CatsIOSyntax
-}
